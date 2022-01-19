705be6ca49f4debbb1dfb03649f618eb5f74eb81
BigRichi <big.richi@outlook.com>
2022-01-18 21:13:54 -0500

    Web 3 problem

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,26 @@ pragma solidity ^0.5.0;
 contract TodoList {
     //----> state variable
     uint public taskCount = 0;
+
+    //----> struct allow us to define our own data types 
+    // solidity is a statictly typed language
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    // constructor function Innit funciton
 }
